682f036038a8ae4e463df83702f0de4d69816e99
Yangungsu <119376049+Yangungsu@users.noreply.github.com>
2023-02-25 16:51:25 +0900

    211fd1d5f15d

--- js/sub.js
+++ js/sub.js
@@ -1,16 +1,16 @@
 //슬라이더구좌
-$(function(){
-    $(".prev").click(function(){
+$(function () {
+    $(".prev").click(function () {
         $(".book_info_slide_inner li:last").prependTo('.book_info_slide_inner ul');
-        $(".book_info_slide_inner ul").css('margin-left',"-50%");
-        $(".book_info_slide_inner ul").stop().animate({marginLeft:0},800);
+        $(".book_info_slide_inner ul").css('margin-left', "-50%");
+        $(".book_info_slide_inner ul").stop().animate({ marginLeft: 0 }, 800);
     });
 
-    $(".next").click(function(){
-       $(".book_info_slide_inner ul").stop().animate({marginLeft:"-50%"},800,function(){
-        $(".book_info_slide_inner li:first").appendTo('.book_info_slide_inner ul');
-        $(".book_info_slide_inner ul").css({marginLeft:0});
-       })
+    $(".next").click(function () {
+        $(".book_info_slide_inner ul").stop().animate({ marginLeft: "-50%" }, 800, function () {
+            $(".book_info_slide_inner li:first").appendTo('.book_info_slide_inner ul');
+            $(".book_info_slide_inner ul").css({ marginLeft: 0 });
+        })
     });
 })
 
@@ -36,31 +36,157 @@ $.ajax({
         }
     });
 
-    $.ajax({
-        method: "GET",
-        url: "https://dapi.kakao.com/v3/search/book?target=title",
-        data: { query: "김미경" },
-        headers: { Authorization: "KakaoAK d37f4a4b153563c9647a2d78226dac2a" }
-    })
-    
-        .done(function (msg) {
-            var boxs = document.getElementsByClassName('swiper-slide');
-            for (var i = 0; i < boxs.length; i++) {
-                var str = msg.documents[i].title;
-                var str3 = msg.documents[i].price;
-    
-                var str2 = str.substring(0, 20);
-                $(".api_h").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>")
-                    .append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
-            }
-        });
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "김미경" },
+    headers: { Authorization: "KakaoAK d37f4a4b153563c9647a2d78226dac2a" }
+})
 
-//book_long_text 불러오기
-    $(function () {
-        $.get("./text/text.txt", function (data) {
-            $(".long_text").html(data);
-        })
+    .done(function (msg) {
+        var boxs = document.getElementsByClassName('swiper-slide');
+        for (var i = 0; i < boxs.length; i++) {
+            var str = msg.documents[i].title;
+            var str3 = msg.documents[i].price;
+
+            var str2 = str.substring(0, 20);
+            $(".api_h").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>")
+                .append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
+        }
     });
+
+
+//right_inner_api
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "김미경의 마흔" },
+    headers: { Authorization: "KakaoAK d37f4a4b153563c9647a2d78226dac2a" }
+})
+
+    .done(function (msg) {
+        var boxs = document.getElementsByClassName('column_api_inner_api_area');
+        for (var i = 0; i < boxs.length; i++) {
+            var str = msg.documents[i].title;
+            var str3 = msg.documents[i].price;
+
+            var str2 = str.substring(0, 20);
+            $(".column_api_inner_api_img").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
+            $(".column_api_inner_api_text").append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
+                .append("<h5>" + msg.documents[i].authors + "</h5>")
+                .append("<h6>" + str3 + "</h6>");
+        }
+    });
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "1퍼센트의 부자 법칙" },
+    headers: { Authorization: "KakaoAK d37f4a4b153563c9647a2d78226dac2a" }
+})
+
+    .done(function (msg) {
+        var boxs = document.getElementsByClassName('column_api_inner_api_area1');
+        for (var i = 0; i < boxs.length; i++) {
+            var str = msg.documents[i].title;
+            var str3 = msg.documents[i].price;
+
+            var str2 = str.substring(0, 20);
+            $(".column_api_inner_api_img1").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
+            $(".column_api_inner_api_text1").append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
+                .append("<h5>" + msg.documents[i].authors + "</h5>")
+                .append("<h6>" + str3 + "</h6>");
+        }
+    });
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "세이노의 가르침" },
+    headers: { Authorization: "KakaoAK d37f4a4b153563c9647a2d78226dac2a" }
+})
+
+    .done(function (msg) {
+        var boxs = document.getElementsByClassName('column_api_inner_api_area2');
+        for (var i = 0; i < boxs.length; i++) {
+            var str = msg.documents[i].title;
+            var str3 = msg.documents[i].price;
+
+            var str2 = str.substring(0, 20);
+            $(".column_api_inner_api_img2").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
+            $(".column_api_inner_api_text2").append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
+            .append("<h5>" + msg.documents[i].authors + "</h5>")
+            .append("<h6>" + str3 + "</h6>");
+        }
+    });
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "원씽" },
+    headers: { Authorization: "KakaoAK d37f4a4b153563c9647a2d78226dac2a" }
+})
+
+    .done(function (msg) {
+        var boxs = document.getElementsByClassName('column_api_inner_api_area3');
+        for (var i = 0; i < boxs.length; i++) {
+            var str = msg.documents[i].title;
+            var str3 = msg.documents[i].price;
+
+            var str2 = str.substring(0, 20);
+            $(".column_api_inner_api_img3").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
+               $(".column_api_inner_api_text3").append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
+                .append("<h5>" + msg.documents[i].authors + "</h5>")
+                .append("<h6>" + str3 + "</h6>");
+        }
+    });
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "역행자" },
+    headers: { Authorization: "KakaoAK d37f4a4b153563c9647a2d78226dac2a" }
+})
+
+    .done(function (msg) {
+        var boxs = document.getElementsByClassName('column_api_inner_api_area4');
+        for (var i = 0; i < boxs.length; i++) {
+            var str = msg.documents[i].title;
+            var str3 = msg.documents[i].price;
+
+            var str2 = str.substring(0, 20);
+            $(".column_api_inner_api_img4").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
+            $(".column_api_inner_api_text4").append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
+                .append("<h5>" + msg.documents[i].authors + "</h5>")
+                .append("<h6>" + str3 + "</h6>");
+        }
+    });
+
+//right_inner_api_end
+//book_long_text 불러오기
+$(function () {
+    $.get("./text/text.txt", function (data) {
+        $(".long_text").html(data);
+    })
+});
+
+$(function () {
+    $.get("./text/booklist.txt", function (data) {
+        $(".over_flow_wrap_content").html(data);
+    })
+});
+
+$(function () {
+    $.get("./text/bookgoing.txt", function (data) {
+        $(".over_flow_wrap_content2").html(data);
+    })
+});
+
+$(function () {
+    $.get("./text/bookwork.txt", function (data) {
+        $(".over_flow_wrap_content3").html(data);
+    })
+});
 
 //end    
 
@@ -82,7 +208,7 @@ $(function () {
 //     }else if($(this).find('.b_text').text()=='접기'){
 //         $(this).siblings('p').css({'height':'67px','overflow':'hidden'});
 //         $(this).children('span').text('펼치기').children('img').css({'transform':'rotate(0deg)'});
-//     }    
+//     }
 // });
 
 
