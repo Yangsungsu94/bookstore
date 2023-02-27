d4bb8ea301ee2521e1a8a1e2cf44bc31769de4d2
Yangungsu <119376049+Yangungsu@users.noreply.github.com>
2023-02-27 20:31:03 +0900

    20323-02-27

--- js/sub.js
+++ js/sub.js
@@ -74,7 +74,7 @@ $.ajax({
             $(".column_api_inner_api_img").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
             $(".column_api_inner_api_text").append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
                 .append("<h5>" + msg.documents[i].authors + "</h5>")
-                .append("<h6>" + str3 + "</h6>");
+                .append("<h6>" + "<b>" + "10%" + "</b>" + str3 + "원</h6>");
         }
     });
 
@@ -95,7 +95,7 @@ $.ajax({
             $(".column_api_inner_api_img1").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
             $(".column_api_inner_api_text1").append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
                 .append("<h5>" + msg.documents[i].authors + "</h5>")
-                .append("<h6>" + str3 + "</h6>");
+                .append("<h6>" + "<b>" + "10%" + "</b>" + str3 + "원</h6>");
         }
     });
 
@@ -115,8 +115,8 @@ $.ajax({
             var str2 = str.substring(0, 20);
             $(".column_api_inner_api_img2").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
             $(".column_api_inner_api_text2").append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
-            .append("<h5>" + msg.documents[i].authors + "</h5>")
-            .append("<h6>" + str3 + "</h6>");
+                .append("<h5>" + msg.documents[i].authors + "</h5>")
+                .append("<h6>" + "<b>" + "10%" + "</b>" + str3 + "원</h6>");
         }
     });
 
@@ -135,9 +135,9 @@ $.ajax({
 
             var str2 = str.substring(0, 20);
             $(".column_api_inner_api_img3").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
-               $(".column_api_inner_api_text3").append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
+            $(".column_api_inner_api_text3").append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
                 .append("<h5>" + msg.documents[i].authors + "</h5>")
-                .append("<h6>" + str3 + "</h6>");
+                .append("<h6>" + "<b>" + "10%" + "</b>" + str3 + "원</h6>");
         }
     });
 
@@ -158,11 +158,118 @@ $.ajax({
             $(".column_api_inner_api_img4").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
             $(".column_api_inner_api_text4").append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
                 .append("<h5>" + msg.documents[i].authors + "</h5>")
-                .append("<h6>" + str3 + "</h6>");
+                .append("<h6>" + "<b>" + "10%" + "</b>" + str3 + "원</h6>");
         }
     });
 
+
+    $.ajax({
+        method: "GET",
+        url: "https://dapi.kakao.com/v3/search/book?target=title",
+        data: { query: "부의" },
+        headers: { Authorization: "KakaoAK d37f4a4b153563c9647a2d78226dac2a" }
+    })
+    
+        .done(function (msg) {
+            var boxs = document.getElementsByClassName('column_api_inner_api_area4_zone');
+            for (var i = 0; i < boxs.length; i++) {
+                var str = msg.documents[i].title;
+                var str3 = msg.documents[i].price;
+    
+                var str2 = str.substring(0, 20);
+                $(".column_api_inner_api_img4_zone").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
+                $(".column_api_inner_api_text4_zone").append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
+                    .append("<h5>" + msg.documents[i].authors + "</h5>")
+                    .append("<h6>" + "<b>" + "10%" + "</b>" + str3 + "원</h6>");
+            }
+        });
+
+        $.ajax({
+            method: "GET",
+            url: "https://dapi.kakao.com/v3/search/book?target=title",
+            data: { query: "부를 움켜쥐는 17가지 법칙" },
+            headers: { Authorization: "KakaoAK d37f4a4b153563c9647a2d78226dac2a" }
+        })
+        
+            .done(function (msg) {
+                var boxs = document.getElementsByClassName('column_api_inner_api_area3_zone');
+                for (var i = 0; i < boxs.length; i++) {
+                    var str = msg.documents[i].title;
+                    var str3 = msg.documents[i].price;
+        
+                    var str2 = str.substring(0, 20);
+                    $(".column_api_inner_api_img3_zone").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
+                    $(".column_api_inner_api_text3_zone").append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
+                        .append("<h5>" + msg.documents[i].authors + "</h5>")
+                        .append("<h6>" + "<b>" + "10%" + "</b>" + str3 + "원</h6>");
+                }
+            });
+
+            $.ajax({
+                method: "GET",
+                url: "https://dapi.kakao.com/v3/search/book?target=title",
+                data: { query: "생각 레가토(큰글자도서)" },
+                headers: { Authorization: "KakaoAK d37f4a4b153563c9647a2d78226dac2a" }
+            })
+            
+                .done(function (msg) {
+                    var boxs = document.getElementsByClassName('column_api_inner_api_area3_zone');
+                    for (var i = 0; i < boxs.length; i++) {
+                        var str = msg.documents[i].title;
+                        var str3 = msg.documents[i].price;
+            
+                        var str2 = str.substring(0, 20);
+                        $(".column_api_inner_api_img2_zone").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
+                        $(".column_api_inner_api_text2_zone").append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
+                            .append("<h5>" + msg.documents[i].authors + "</h5>")
+                            .append("<h6>" + "<b>" + "10%" + "</b>" + str3 + "원</h6>");
+                    }
+                });
+
+                $.ajax({
+                    method: "GET",
+                    url: "https://dapi.kakao.com/v3/search/book?target=title",
+                    data: { query: "이제 막 출근했는데, 뭘 하라고요?" },
+                    headers: { Authorization: "KakaoAK d37f4a4b153563c9647a2d78226dac2a" }
+                })
+                
+                    .done(function (msg) {
+                        var boxs = document.getElementsByClassName('column_api_inner_api_area3_zone');
+                        for (var i = 0; i < boxs.length; i++) {
+                            var str = msg.documents[i].title;
+                            var str3 = msg.documents[i].price;
+                
+                            var str2 = str.substring(0, 20);
+                            $(".column_api_inner_api_img1_zone").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
+                            $(".column_api_inner_api_text1_zone").append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
+                                .append("<h5>" + msg.documents[i].authors + "</h5>")
+                                .append("<h6>" + "<b>" + "10%" + "</b>" + str3 + "원</h6>");
+                        }
+                    });
+
+                    $.ajax({
+                        method: "GET",
+                        url: "https://dapi.kakao.com/v3/search/book?target=title",
+                        data: { query: "영어 잘하는 사람보다 매일 하는 사람" },
+                        headers: { Authorization: "KakaoAK d37f4a4b153563c9647a2d78226dac2a" }
+                    })
+                    
+                        .done(function (msg) {
+                            var boxs = document.getElementsByClassName('column_api_inner_api_area3_zone');
+                            for (var i = 0; i < boxs.length; i++) {
+                                var str = msg.documents[i].title;
+                                var str3 = msg.documents[i].price;
+                    
+                                var str2 = str.substring(0, 20);
+                                $(".column_api_inner_api_img_zone").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
+                                $(".column_api_inner_api_text_zone").append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
+                                    .append("<h5>" + msg.documents[i].authors + "</h5>")
+                                    .append("<h6>" + "<b>" + "10%" + "</b>" + str3 + "원</h6>");
+                            }
+                        });
+
 //right_inner_api_end
+
 //book_long_text 불러오기
 $(function () {
     $.get("./text/text.txt", function (data) {
@@ -188,6 +295,41 @@ $(function () {
     })
 });
 
+$(function () {
+    $.get("./text/change.txt", function (data) {
+        $(".as_change").html(data);
+    })
+});
+
+$(function () {
+    $.get("./text/changetime.txt", function (data) {
+        $(".as_change_time").html(data);
+    })
+});
+
+$(function () {
+    $.get("./text/changeprice.txt", function (data) {
+        $(".as_change_price").html(data);
+    })
+});
+
+$(function () {
+    $.get("./text/changereason.txt", function (data) {
+        $(".as_change_reason").html(data);
+    })
+});
+
+$(function () {
+    $.get("./text/changesoldout.txt", function (data) {
+        $(".as_change_soldout").html(data);
+    })
+});
+
+$(function () {
+    $.get("./text/changecompensation.txt", function (data) {
+        $(".as_change_reason_compensation").html(data);
+    })
+});
 //end    
 
 //top swiper
@@ -200,16 +342,38 @@ $(function () {
 //end
 
 // 메뉴접고펼치기
+$(function () {
+    $('.profile_text_button').click(function () {
+        console.log($(this).find('.b_text').text() === '펼치기')
+        if ($(this).find('.b_text').text() === '펼치기') {
+            $(this).siblings().find('p').css({ 'overflow': 'visible','white-space': 'normal' });
+            $(this).find('.b_text').text('접기').siblings().find('img').css({ 'transform': 'rotate(180deg)' });
+        } else if ($(this).find('.b_text').text() == '접기') {
+            $(this).siblings().find('p').css({ 'overflow': 'hidden','white-space': 'nowrap' });
+            $(this).find('.b_text').text('펼치기').siblings().find('img').css({ 'transform': 'rotate(0deg)' });
+        }
+    });
+});
 
-// $('.profile_text_button').click(function(){
-//     if($(this).find('.b_text').text()=='펼치기'){
-//         $(this).siblings().css({'height':'100%', 'overflow':'visible'});
-//         $(this).children('.b_text').text('접기').find('img').css({'transform':'rotate(180deg)'});
-//     }else if($(this).find('.b_text').text()=='접기'){
-//         $(this).siblings('p').css({'height':'67px','overflow':'hidden'});
-//         $(this).children('span').text('펼치기').children('img').css({'transform':'rotate(0deg)'});
-//     }
-// });
+$(function () {
+    $('.over_flow_button').click(function () {
+        console.log($(this).find('.c_text').text() === '펼치기')
+        if ($(this).find('.c_text').text() === '펼치기') {
+            $(this).siblings().find('p').css({ 'overflow': 'visible','white-space': 'normal' });
+            $(this).find('.c_text').text('접기').siblings().find('img').css({ 'transform': 'rotate(180deg)' });
+        } else if ($(this).find('.c_text').text() == '접기') {
+            $(this).siblings().find('p').css({ 'overflow': 'hidden','white-space': 'nowrap' });
+            $(this).find('.c_text').text('펼치기').siblings().find('img').css({ 'transform': 'rotate(0deg)' });
+        }
+    });
+})
+
+
+
+
+// 자동계산
+
+
 
 
 
